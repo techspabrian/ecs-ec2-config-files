5b145c0ea907c59c92918263f6b552b60ea5e281
Brian Cotton <brian.cotton@technologyspa.com>
2019-06-18 17:40:32 -0500

    adding region to script

--- createLogGroups.sh
+++ createLogGroups.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep '\[' ecs-ec2-config-files/awslogs.conf | grep -v general | sed /"\["/s/"\["/''/  |sed /"\]"/s/"\]"/''/ | while read LOG; do aws logs create-log-group --log-group-name  $LOG; done
+grep '\[' ecs-ec2-config-files/awslogs.conf | grep -v general | sed /"\["/s/"\["/''/  |sed /"\]"/s/"\]"/''/ | while read LOG; do aws logs create-log-group --log-group-name  $LOG --region REGION; done
